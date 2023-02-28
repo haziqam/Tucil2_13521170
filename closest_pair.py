c7a427ba555473373cd78e25b43804a3f5059e06
Haziq <haziqabiyyu@gmail.com>
2023-03-01 00:30:30 +0700

    Dokumentasi program, komentar, validasi input, dan jumlah operasi

--- closest_pair.py
+++ closest_pair.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 
 
 def generate_random_points(N, dimensions, threshold=1000):
+    """
+    Mengembalikan numpy.NDArray berisi N buah titik acak dengan dimensi sebesar dimensions.
+    Seluruh titik akan berada pada range
+    -1 * threshold <= x < threshold,
+    -1 * threshold <= y < threshold,
+    -1 * threshold <= z < threshold, 
+    dst.
+    """
     arr_points = np.empty(N, dtype=object)
     
     for i in range(N):
@@ -21,6 +29,9 @@ def generate_random_points(N, dimensions, threshold=1000):
 
 
 def euclidean_dist(point_a, point_b, dimensions): 
+    """
+    Mengembalikan jarak euclidean antara titik a dan titik b dengan dimensi sebesar dimensions
+    """
     result = 0
     for i in range(dimensions):
         result += (point_a[i] - point_b[i])**2
@@ -31,34 +42,53 @@ def euclidean_dist(point_a, point_b, dimensions):
 
 
 def bf_closest_pair(arr_points, dimensions): 
+    """
+    Mengembalikan jarak terpendek, pasangan titik terdekat, serta banyaknya operasi 
+    perhitungan jarak euclidean yang dilakukan dengan algoritma brute force
+    Prekondisi: 
+    len(arr_points) >= 2 dan dimensions >=1
+    """
     closest_pair = None
     min_dist = float('inf')
+    operations = 0
 
     for i in range(0, len(arr_points)):
         for j in range(i+1, len(arr_points)):
             current_dist = euclidean_dist(arr_points[i], arr_points[j], dimensions) 
+            operations += 1
             if (current_dist < min_dist):
                 min_dist = current_dist
                 closest_pair = (arr_points[i], arr_points[j])
 
-    return min_dist, closest_pair
+    return min_dist, closest_pair, operations
 
 
 
 def dnc_closest_pair(arr_points, dimensions):
+    """
+    Mengembalikan jarak terpendek, pasangan titik terdekat, serta banyaknya operasi 
+    perhitungan jarak euclidean yang dilakukan dengan algoritma divide and conquer.
+    Prekondisi: 
+    len(arr_points) >= 2, dimensions >=1, dan arr_points
+    sudah terurut berdasarkan absis 
+    """
     num_points = len(arr_points)
 
     if (num_points <= 3):
         return bf_closest_pair(arr_points, dimensions)
 
+    # Pengambilan titik tengah yang dapat membagi array menjadi partisi kiri dan kanan sama rata
+    # dengan elemen tengah array sebagai acuan.
     mid = num_points // 2
     mid_x = arr_points[mid][0]
     left_arr_points = arr_points[:mid]
     right_arr_points = arr_points[mid:]
 
-    left_min_dist, left_closest_pair = dnc_closest_pair(left_arr_points, dimensions)
-    right_min_dist, right_closest_pair = dnc_closest_pair(right_arr_points, dimensions)
+    # Pencarian pasangan terdekat pada partisi kiri dan kanan secara rekursif
+    left_min_dist, left_closest_pair, left_operations = dnc_closest_pair(left_arr_points, dimensions)
+    right_min_dist, right_closest_pair, right_operations = dnc_closest_pair(right_arr_points, dimensions)
 
+    # Jarak terpendek sementara adalah min(left_min_dist, right_min_dist)
     if (left_min_dist <= right_min_dist):
         closest_pair = left_closest_pair
         min_dist = left_min_dist
@@ -66,26 +96,43 @@ def dnc_closest_pair(arr_points, dimensions):
         closest_pair = right_closest_pair
         min_dist = right_min_dist
 
+    # Titik-titik yang jarak absisnya dengan mid_x <= min_dist akan dimasukkan ke dalam slab
     slab = []
     slab_width = min_dist
+    slab_operations = 0
 
     for point in arr_points:
         if (abs(point[0] - mid_x) <= slab_width):
             slab.append(point)
 
+    # Pencarian pasangan terdekat untuk titik-titik yang berada dalam slab
     for i in range(0, len(slab)):
         for j in range(i + 1, len(slab)):
+            # Jika terdapat sepasang titik di dalam slab yang tidak memenuhi kriteria close_enough 
+            # (lihat fungsi di bawah) maka tidak perlu dilakukan perhitungan 
+            # jarak euclidean, karena sudah pasti jaraknya > min_dist
             if (close_enough(slab[i], slab[j], slab_width, dimensions)):
                 current_dist = euclidean_dist(slab[i], slab[j], dimensions)
+                slab_operations += 1
                 if (current_dist < min_dist):
                     min_dist = current_dist
                     closest_pair = (slab[i], slab[j])
         
-    return min_dist, closest_pair
+    total_operations = left_operations + right_operations + slab_operations
+    return min_dist, closest_pair, total_operations
 
 
 
 def close_enough(point_a, point_b, dist_limit, dimensions):
+    """
+    Menentukan apakah titik a (x1, y1, z1) dan titik b (x2, y2, z2) 
+    cukup dekat dengan kriteria berikut:
+    |x1 - x2| < dist_limit,
+    |y1 - y2| < dist_limit,
+    |z1 - z2| < dist_limit,
+    dst.
+    Digunakan untuk mengurangi jumlah perhitungan pada algoritma divide and conquer
+    """
     for i in range(1, dimensions):
         if (abs(point_a[i] - point_b[i]) > dist_limit):
             return False
@@ -95,6 +142,12 @@ def close_enough(point_a, point_b, dist_limit, dimensions):
 
 
 def plot_results(arr_points, closest_pair):
+    """
+    Melakukan plotting terhadap titik-titik yang ada pada arr_points. pasangan titik 
+    yang merupakan pasangan terdekat diberi warna merah, sedangkan yang lainnya
+    diberi warna hijau
+
+    """
     fig = plt.figure(figsize = (16, 9))
     ax = plt.axes(projection ="3d")
     ax.set_xlabel('X')
@@ -112,6 +165,9 @@ def plot_results(arr_points, closest_pair):
 
 
 def quick_sort_by_x(arr_points, left, right):
+    """
+    Melakukan sorting pada arr_points dengan algoritma quick sort
+    """
     if (left < right):
         pivot = arr_points[right][0]
         i = left - 1
--- main.py
+++ main.py
@@ -2,25 +2,51 @@ import time
 import closest_pair as cp
 
 
-arr_points = cp.generate_random_points(1000, 3, threshold=1000)
+print("====================== INPUTS =======================")
+while (True):
+    num_points = int(input("Number of points: "))
+    if (num_points >=2):
+        break
+    print("Number of points must be greater than or equal to 2")
+
+while (True):
+    dim = int(input("Dimensions: "))
+    if (dim >= 1):
+        break
+    print("Dimensions must be greater than or equal to 1")
+
+
+print("=====================================================")
+print("\n")
+
+arr_points = cp.generate_random_points(num_points, dim, threshold=1000)
 
 bf_start_time = time.time()
-bf_min_dist, bf_closest_pair = cp.bf_closest_pair(arr_points, 3)
+bf_min_dist, bf_closest_pair, bf_operations = cp.bf_closest_pair(arr_points, dim)
 bf_finish_time = time.time()
 
-print("Brute Force: ")
+print("==================== BRUTE FORCE ====================")
 print("Min distance:", bf_min_dist)
 print("Closest pair:", bf_closest_pair)
 print("Execution time:", bf_finish_time - bf_start_time)
+print("Total operations:", bf_operations)
+print("=====================================================")
 
+print("\n")
+         
 dnc_start_time = time.time()
 cp.quick_sort_by_x(arr_points, 0, len(arr_points) - 1)
-dnc_min_dist, dnc_closest_pair = cp.dnc_closest_pair(arr_points, 3)
+dnc_min_dist, dnc_closest_pair, dnc_operations = cp.dnc_closest_pair(arr_points, dim)
 dnc_finish_time = time.time()
 
-print("Divide and Conquer: ")
+print("================ DIVIDE AND CONQUER =================")
 print("Min distance:", dnc_min_dist)
 print("Closest pair: ", dnc_closest_pair)
 print("Execution time:", dnc_finish_time - dnc_start_time)
+print("Total operations:", dnc_operations)
+print("=====================================================")
 
-#cp.plot_results(arr_points, dnc_closest_pair)
+if (dim == 3):
+    ans = input("Plot results? (y/n): ")
+    if (ans == "y"):
+        cp.plot_results(arr_points, dnc_closest_pair)
